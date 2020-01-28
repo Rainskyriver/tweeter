f56ef3bbb486ef39aa99107d569b442f1e3701df
Lian Yelich <lianyelich14@gmail.com>
2020-01-28 01:41:13 +0000

    Add char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  console.log('jQuery is Ready')
+  $('textarea').keyup(function() {
+    $(this).parent().find('.counter').text(140 - ($(this).val().length));
+    let onOrOff = true;
+    if (parseInt($(this).parent().find('.counter').text()) <= 0) {
+      onOrOff = true;
+    } else {
+      onOrOff = false;
+    }
+    $(this).parent().find('.counter').toggleClass("below-zero", onOrOff);
+  });
+});
