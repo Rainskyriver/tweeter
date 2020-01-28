a7f48434e9dcc9950df6a44ae42161d3447c3f51
Lian Yelich <lianyelich14@gmail.com>
2020-01-28 23:51:30 +0000

    Add loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,47 +4,40 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const data = [
-    {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIssac"
-    },
-    "content": {
-      "text": "If I have seen further, it is by standing on the shoulders of giants"
-    },
-    "created at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense, donc je suis"
-    },
-    "created at": 1461116232226
-  }
-];
+
+  $('form').submit((event) => {
+    console.log('event:', event);
+    event.preventDefault();
+  })
+
+  
   const createTweetElement = function(data) {
     const $returnTweet = $("<article>").addClass("tweet");
     const name = data.user.name;
     const handle = data.user.handle;
     const content = data.content.text;
-    $returnTweet.append(`<header>${name}<span>${handle}</span></header>`)
-    $returnTweet.append(`<span>${content}</span>`)
-    $returnTweet.append(`<hr>`)
-
+    $returnTweet.append(`
+    <header>${name}<span>${handle}</span></header>
+    <span>${content}</span>
+    <hr>
+    <footer>${data["created_at"]}</footer>
+    `)
     return $returnTweet;
   }
-
+  
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $('#tweet-container').append($tweet);
     }
   }
-  renderTweets(data);
+  
+  const loadTweets = function() {
+    $.ajax('http://localhost:8080/tweets', { method: 'GET'})
+    .then((tweets) => {
+      renderTweets(tweets);
+    })
+  }
+
+  loadTweets();
 });
