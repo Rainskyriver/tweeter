af47bad24ec58feaf15575edbdfeb56e30941d04
Lian Yelich <lianyelich14@gmail.com>
2020-01-28 22:30:39 +0000

    Add createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,17 +3,31 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetObject = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIssac"
-  },
-  "content": {
-    "text": "If I have seen further, it is by standing on the shoulders of giants"
-  },
-  "created at": 1461116232227
-};
-const createTweetElement = function(data) {
+$(document).ready(function() {
+  const tweetObject = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIssac"
+    },
+    "content": {
+      "text": "If I have seen further, it is by standing on the shoulders of giants"
+    },
+    "created at": 1461116232227
+  };
+  const createTweetElement = function(data) {
+    const $returnTweet = $("<article>").addClass("tweet");
+    const name = data.user.name;
+    const handle = data.user.handle;
+    const content = data.content.text;
+    $returnTweet.append(`<header>${name}<span>${handle}</span></header>`)
+    $returnTweet.append(`<span>${content}</span>`)
+    $returnTweet.append(`<hr>`)
 
-}
+
+    return $returnTweet;
+  }
+  const $tweet = createTweetElement(tweetObject);
+  console.log($tweet);
+  $('#tweet-container').append($tweet);
+});
