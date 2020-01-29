946638ea0049579753ae037e96ec09474739e5a7
Lian Yelich <lianyelich14@gmail.com>
2020-01-29 01:17:28 +0000

    Add post functionality and reload without refresh on posting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,10 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-
-  $('form').submit((event) => {
-    console.log('event:', event);
+  
+  $('form').submit(function(event) {
     event.preventDefault();
+    const tweetText = ($(this).serialize());
+    const $textarea = $('textarea')
+    //above 140 characters
+    if ($textarea.val().length > 140) {
+      alert('TOO LONG Stop! You violated the law. Pay the court a fine or server your sentence. Your stolen goods are now forfeit.')
+    }
+    //empty text
+    if ($textarea.val() === '') {
+      alert('EMPTY Stop! You violated the law. Pay the court a fine or server your sentence. Your stolen goods are now forfeit.')
+    }
+
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: tweetText
+    })
+    .done(() => {
+      $('#tweet-container').empty();
+      loadTweets();
+    })
+
+    //clear and keep focus
+    $textarea.val('').focus();
+    //reload tweets without refreshing
+    // setTimeout(() => {
+    // })
   })
 
   
@@ -17,7 +42,8 @@ $(document).ready(function() {
     const handle = data.user.handle;
     const content = data.content.text;
     $returnTweet.append(`
-    <header>${name}<span>${handle}</span></header>
+    
+    <header><img src="${data.user.avatars}">${name}<span>${handle}</span></header>
     <span>${content}</span>
     <hr>
     <footer>${data["created_at"]}</footer>
